b0514c5ae6e52814bab075e8dc952b4b96149dfe
Ivan_Fan <Ivan_Fan@epam.com>
2018-07-06 17:45:43 +0800

    update for react

--- NodeJs/HttpDemo1.js
+++ NodeJs/HttpDemo1.js
@@ -1,62 +1,15 @@
-const https = require('https');
+setImmediate(()=>{
+  console.log('setImmediate1')
+  setTimeout(()=>{
+    console.log('setTimeout1')    
+  },0)
+})
+setTimeout(()=>{
+  console.log('setTimeout2') 
+  process.nextTick(()=>{console.log('nextTick1')})
+  setImmediate(()=>{
+    console.log('setImmediate2')
+  })   
+},10)
 
-https.get('https://encrypted.google.com/', (res) => {
-  console.log('statusCode: ', res.statusCode);
-  console.log('headers: ', res.headers);
-
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-
-}).on('error', (e) => {
-  console.error(e);
-}); <ContentPage.Resources>
-    <ResourceDictionary>
-      <Style x:Key="baseStyle" TargetType="View">
-        <Setter Property="HorizontalOptions" Value="Center" />
-        <Setter Property="VerticalOptions" Value="CenterAndExpand"/>
-        <Setter Property="BackgroundColor" Value="#FFFFFF"/>
-      </Style>
-      <Style x:Key="labelStyle" TargetType="Label">
-        <Setter Property="FontSize" Value="Medium"></Setter>
-        <Setter Property="TextColor" Value="#949494"></Setter>
-      </Style>
-      <Style x:Key="editorStyle" TargetType="Editor">
-        <Setter Property="HeightRequest" Value="50"/>
-        <Setter Property="TextColor" Value="#A5A5A5"/>
-        <Setter Property="BackgroundColor" Value="White"/>
-      </Style>
-      <Style x:Key="buttonSeleted" TargetType="Button">
-        <Setter Property="BackgroundColor" Value="#00CCCC"></Setter>
-        <Setter Property="HeightRequest" Value="40"></Setter>
-      </Style>
-      <Style x:Key="buttonUnseleted" TargetType="Button">
-        <Setter Property="BackgroundColor" Value="#E5E5E5"></Setter>
-        <Setter Property="HeightRequest" Value="40"></Setter>
-      </Style>
-    </ResourceDictionary>
-  </ContentPage.Resources>
-  <ContentPage.Content Style="{StaticResource baseStyle}">
-    <StackLayout Padding="15, 30, 15, 10" BackgroundColor="#FFFFFF">
-      <Label Text="{Binding Title}" Style="{StaticResource labelStyle}"/>
-      <optionTemplate:TextAndTextBox BindingContext="{Binding Options[0]}"/>
-      <optionTemplate:TextAndTextBox BindingContext="{Binding Options[1]}"/>
-      <Grid>
-        <Grid.ColumnDefinitions>
-          <ColumnDefinition Width="5*"></ColumnDefinition>
-          <ColumnDefinition Width="5*"></ColumnDefinition>
-        </Grid.ColumnDefinitions>
-        <Grid.RowDefinitions>
-          <RowDefinition Height="*" />
-        </Grid.RowDefinitions>
-        <ContentView Grid.Column="0" Grid.Row="1" Padding="40,35,40,0">
-          <Button x:Name="prevButton"  Text="Prev" Style="{StaticResource buttonUnseleted}" Clicked="OnPrev"></Button>
-        </ContentView>
-        <ContentView Grid.Column="1" Grid.Row="1" Padding="40,35,40,0">
-          <Button x:Name="nextButton" Text="NEXT" Style="{StaticResource buttonSeleted}"  Clicked="OnNext"></Button>
-        </ContentView>
-      </Grid>
-    </StackLayout>
-  </ContentPage.Content>
-  
-</ContentPage>
+ 
--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 var originCreateElement = document.createElement;
 document.createElement = function() {
     if (arguments[0] === 'span'){
@@ -10,6 +11,7 @@ document.createElement = function() {
     }
    return originCreateElement.apply(document, arguments);
 }
+
 ReactDOM.render(<App />, document.getElementById('root'), function(){
     console.log('react dom render')
     console.log(this)
