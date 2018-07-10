427db1934b851f04efd38acb53e5f2ba22daa380
tomfanxiaojun <594774261@qq.com>
2018-07-10 23:13:52 +0800

    add koa experss

---
+++ NodeJs/koa/sourceNote/koa-compose/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+/**
+ * Expose compositor.
+ */
+
+module.exports = compose
+
+/**
+ * Compose `middleware` returning
+ * a fully valid middleware comprised
+ * of all those which are passed.
+ * 1，compose 传入一个中间件数组
+ * 2，返回一个
+ * @param {Array} middleware
+ * @return {Function}
+ * @api public
+ */
+
+function compose (middleware) {
+  if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+  for (const fn of middleware) {
+    if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+  }
+
+  /**
+   * @param {Object} context
+   * @return {Promise}
+   * @api public
+   */
+
+  return function (context, next) {
+    // last called middleware #
+    let index = -1
+    return dispatch(0)// 自执行第一个中间件
+    function dispatch (i) {
+      if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+      index = i
+      let fn = middleware[i]
+      if (i === middleware.length) fn = next
+      if (!fn) return Promise.resolve()
+      try {
+        // 最重要的逻辑， dispatch.bind(null, i + 1) 相当于next,下一个要执行的中间件
+
+        return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
+      } catch (err) {
+        return Promise.reject(err)
+      }
+    }
+  }
+}
---
+++ NodeJs/koa/sourceNote/koa/application.js
@@ -0,0 +1,257 @@
+
+'use strict';
+const isGeneratorFunction = require('is-generator-function');
+const debug = require('debug')('koa:application');
+const onFinished = require('on-finished');
+const response = require('./response');
+const compose = require('koa-compose');
+const isJSON = require('koa-is-json');
+const context = require('./context');
+const request = require('./request');
+const statuses = require('statuses');
+const Cookies = require('cookies');
+const accepts = require('accepts');
+const Emitter = require('events');
+const assert = require('assert');
+const Stream = require('stream');
+const http = require('http');
+const only = require('only');
+const convert = require('koa-convert');
+const deprecate = require('depd')('koa');
+
+/**
+ * Expose `Application` class.
+ * Inherits from `Emitter.prototype`.
+ * 继承NodeJS events 模块， NodeJS 大部分模块都是继承events
+ */
+
+module.exports = class Application extends Emitter { 
+  constructor() {
+    super();
+    this.proxy = false;
+    // 保存所有的中间件
+    this.middleware = [];
+    this.subdomainOffset = 2;
+    this.env = process.env.NODE_ENV || 'development';
+    this.context = Object.create(context);
+    this.request = Object.create(request);
+    this.response = Object.create(response);
+  }
+
+  /**
+   * Shorthand for:
+   *
+   *    http.createServer(app.callback()).listen(...)
+   * 1，listen 方法，实现了创建server 而且监听端口
+   * 2，使用的是Node JS内置的模块http
+   * 3，其中最重要的是：callback 回调函数
+   * 4，http.createServer中的回调函数，每次调用启用的对应的资源，都会先进入这个函数，
+   * 5，所以这个函数可以做很多的拦截预处理，也是中间件实现的基本原理
+   *
+   * @param {Mixed} ...
+   * @return {Server}
+   * @api public
+   */
+
+  listen(...args) {
+    debug('listen');
+    const server = http.createServer(this.callback());
+    return server.listen(...args);
+  }
+
+  /**
+   * Return JSON representation.
+   * We only bother showing settings.
+   *
+   * @return {Object}
+   * @api public
+   */
+
+  toJSON() {
+    return only(this, [
+      'subdomainOffset',
+      'proxy',
+      'env'
+    ]);
+  }
+
+  /**
+   * Inspect implementation.
+   *
+   * @return {Object}
+   * @api public
+   */
+
+  inspect() {
+    return this.toJSON();
+  }
+
+  /**
+   * Use the given middleware `fn`.
+   *
+   * Old-style middleware will be converted.
+   * 1， use 用来添加中间件，this.middleware.push(fn);保存在服务实例的middleware的数组中
+   * 2， 参数必须是一个function 
+   * 3，判断function 是否是一个generator function,如果不是，则将转换成generator function.
+   * @param {Function} fn
+   * @return {Application} self
+   * @api public
+   */
+
+  use(fn) {
+    if (typeof fn !== 'function') throw new TypeError('middleware must be a function!');
+    if (isGeneratorFunction(fn)) {
+      deprecate('Support for generators will be removed in v3. ' +
+                'See the documentation for examples of how to convert old middleware ' +
+                'https://github.com/koajs/koa/blob/master/docs/migration.md');
+      fn = convert(fn);
+    }
+    debug('use %s', fn._name || fn.name || '-');
+    this.middleware.push(fn);
+    return this;
+  }
+
+  /**
+   * Return a request handler callback
+   * for node's native http server.
+   * 1，通过compose 方法，递归执行中间件
+   * 2，返回一个handlerequest 方法，
+   * 3，根据req, res 创建一个上下文 const ctx = this.createContext(req, res);
+   * 该上下文其实就是想原生的http 的req 和res做一次映射，映射到koa 的上下文，便于操作
+   * 4，handleRequest ,会在每一次请求之前运行该方法
+   * 5，相当于是createServer的回调函数
+      const server = http.createServer((req, res) => {
+        const ip = res.socket.remoteAddress;
+        const port = res.socket.remotePort;
+        res.end(`你的IP地址是 ${ip}，你的源端口是 ${port}。`);
+      }).listen(3000);
+   * @return {Function}
+   * @api public
+   */
+
+  callback() {
+    const fn = compose(this.middleware);
+    // this.listeners 是继承于events的方法，判断是否有error 错误
+    if (!this.listeners('error').length) this.on('error', this.onerror);
+
+    const handleRequest = (req, res) => {
+      const ctx = this.createContext(req, res);
+      return this.handleRequest(ctx, fn);
+    };
+
+    return handleRequest;
+  }
+
+  /**
+   * Handle request in callback.
+   *
+   * @api private
+   */
+
+  handleRequest(ctx, fnMiddleware) {
+    const res = ctx.res;
+    res.statusCode = 404;
+    const onerror = err => ctx.onerror(err);
+    const handleResponse = () => respond(ctx);
+    onFinished(res, onerror);
+    // 执行完所有的中间件，然后返回response, 也就是http 原生的方式res.end(body);
+    return fnMiddleware(ctx).then(handleResponse).catch(onerror);
+  }
+
+  /**
+   * Initialize a new context.
+   *
+   * @api private
+   */
+
+  createContext(req, res) {
+    const context = Object.create(this.context);
+    const request = context.request = Object.create(this.request);
+    const response = context.response = Object.create(this.response);
+    context.app = request.app = response.app = this;
+    context.req = request.req = response.req = req;
+    context.res = request.res = response.res = res;
+    request.ctx = response.ctx = context;
+    request.response = response;
+    response.request = request;
+    context.originalUrl = request.originalUrl = req.url;
+    context.cookies = new Cookies(req, res, {
+      keys: this.keys,
+      secure: request.secure
+    });
+    request.ip = request.ips[0] || req.socket.remoteAddress || '';
+    context.accept = request.accept = accepts(req);
+    context.state = {};
+    return context;
+  }
+
+  /**
+   * Default error handler.
+   *
+   * @param {Error} err
+   * @api private
+   */
+
+  onerror(err) {
+    assert(err instanceof Error, `non-error thrown: ${err}`);
+
+    if (404 == err.status || err.expose) return;
+    if (this.silent) return;
+
+    const msg = err.stack || err.toString();
+    console.error();
+    console.error(msg.replace(/^/gm, '  '));
+    console.error();
+  }
+};
+
+/**
+ * Response helper.
+ */
+
+function respond(ctx) {
+  // allow bypassing koa
+  if (false === ctx.respond) return;
+
+  const res = ctx.res;
+  if (!ctx.writable) return;
+
+  let body = ctx.body;
+  const code = ctx.status;
+
+  // ignore body
+  if (statuses.empty[code]) {
+    // strip headers
+    ctx.body = null;
+    return res.end();
+  }
+
+  if ('HEAD' == ctx.method) {
+    if (!res.headersSent && isJSON(body)) {
+      ctx.length = Buffer.byteLength(JSON.stringify(body));
+    }
+    return res.end();
+  }
+
+  // status body
+  if (null == body) {
+    body = ctx.message || String(code);
+    if (!res.headersSent) {
+      ctx.type = 'text';
+      ctx.length = Buffer.byteLength(body);
+    }
+    return res.end(body);
+  }
+
+  // responses
+  if (Buffer.isBuffer(body)) return res.end(body);
+  if ('string' == typeof body) return res.end(body);
+  if (body instanceof Stream) return body.pipe(res);
+
+  // body: json
+  body = JSON.stringify(body);
+  if (!res.headersSent) {
+    ctx.length = Buffer.byteLength(body);
+  }
+  res.end(body);
+}
